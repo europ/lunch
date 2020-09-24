65ba052394b1658ae25b27a61427100b443da7f2
Adrián Tóth <adricko28@gmail.com>
2020-09-24 13:07:05 +0200

    hostname

--- lunch/config/environment.rb
+++ lunch/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.application.config.hosts << ENV['HOST_NAME']
+
 # Initialize the Rails application.
 Rails.application.initialize!
